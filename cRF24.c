c4274d66fc7559397bcd579e398c19534fa93a0b
roboclubz <prag.inbox@gmail.com>
2014-02-25 21:10:57 +0530

    addded functionality

--- cRF24.c
+++ cRF24.c
@@ -32,8 +32,7 @@ void rfTransmit(uint8_t* buff, uint8_t len)
 {
     rfToStandbyOne();
     rfWritePayload(buff, len);
-    uint8_t result = rfReadRegChar(CONFIG);
-    rfWriteRegChar(CONFIG, CLEAR_BIT(result, PRIM_RX));
+    rfSetToTransmitMode();
     CE_HIGH;
     _delay_us(10);
     CE_LOW;
@@ -53,18 +52,20 @@ void rfTransmited(void)
     transmittedFlag = 1;
 }
 
-// changes the state of the tranciver to standby mode one
+// changes the state of the transceiver to standby mode one
 void rfToStandbyOne(void)
 {
     rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), PWR_UP));
+    CE_LOW;
 
     _delay_ms(200);
 }
 
-// changes the state of the tranciver to powerDown mode
+// changes the state of the transceiver to powerDown mode
 void rfToPowerDown(void)
 {
     rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), PWR_UP));
+    CE_LOW;
 }
 
 // reads the register and writes to the buffer
@@ -133,6 +134,20 @@ uint8_t rfWritePayload(const uint8_t* buf, uint8_t len)
     return status;
 }
 
+// writes the rfAckPayload from local buffer
+uint8_t rfWriteAckPayload(const uint8_t* buf, uint8_t len)
+{
+    uint8_t status;
+
+    CSN_LOW;
+    status = spiSendData( W_ACK_PAYLOAD );
+    while ( len-- )
+        spiSendData(*buf++);
+    CSN_HIGH;
+
+    return status;
+}
+
 // reads the rfPayload into local buffer
 uint8_t rfReadPayload(uint8_t len)
 {
@@ -150,10 +165,8 @@ uint8_t rfReadPayload(uint8_t len)
 // continuously listens
 void rfStartListening(void)
 {
-    CE_LOW;
-    rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), PRIM_RX));
-    _delay_us(130);
-    CE_HIGH;
+    rfToStandbyOne();
+    rfSetToReceiveMode();
 }
 
 // stops listening
@@ -200,7 +213,327 @@ uint8_t rfStatus(void)
     return status;
 }
 
-ISR(INT0_vect)
+// sets the radio frequency
+void rfSetChannel(uint8_t channel)
+{
+    // check bounds
+    rfWriteRegChar(RF_CH, SET_VALUE(rfReadRegChar(RF_CH), channel));
+}
+
+// sets the transmit power
+void rfSetTransmitPower(uint8_t power)
+{
+    // check bounds
+    rfWriteRegChar(RF_SETUP, SET_VALUE(rfReadRegChar(RF_SETUP), (power<<1)));
+}
+
+// sets the transmit data-rate
+void rfSetDataRate(uint8_t dataRate)
+{
+    // check bounds
+    rfWriteRegChar(RF_SETUP, SET_VALUE(rfReadRegChar(RF_SETUP), (((1<<3) & dataRate)<<3) | ((1<<5) & dataRate<<5)));
+}
+
+// sets receive address width
+void rfSetAddressWidth(uint8_t addressWidth)
+{
+    // check bounds
+    rfWriteRegChar(SETUP_AW, SET_VALUE(rfReadRegChar(SETUP_AW), addressWidth));
+}
+
+/** \brief Enables CRC
+ *
+ * \return NonReturning
+ *
+ */
+void rfEnableCRC(void)
+{
+    rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), EN_CRC));
+}
+
+/** \brief Disables CRC
+ *
+ * \return NonReturning
+ *
+ */
+void rfDisableCRC(void)
+{
+    rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), EN_CRC));
+}
+
+// sets the number of crc bytes
+void rfSetCrcScheme(uint8_t scheme)
+{
+    if (scheme)
+    {
+        rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), CRCO));
+    }
+}
+
+// sets interrupt mask
+void rfSetInterruptMask(uint8_t mask)
+{
+    if (MASK_RX_DR == mask)
+    {
+        rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), MASK_RX_DR));
+    }
+    else if (MASK_TX_DS == mask)
+    {
+        rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), MASK_TX_DS));
+    }
+    else if (MASK_MAX_RT == mask)
+    {
+        rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), MASK_MAX_RT));
+    }
+    else
+    {
+        // something is wrong
+    }
+}
+
+// clears interrupt mask
+void rfClearInterruptMask(uint8_t mask)
+{
+    if (MASK_RX_DR == mask)
+    {
+        rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), MASK_RX_DR));
+    }
+    else if (MASK_TX_DS == mask)
+    {
+        rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), MASK_TX_DS));
+    }
+    else if (MASK_MAX_RT == mask)
+    {
+        rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), MASK_MAX_RT));
+    }
+    else
+    {
+        // something is wrong
+    }
+}
+
+// sets the radio to transmit mode
+void rfSetToTransmitMode(void)
+{
+    CE_LOW;
+    rfWriteRegChar(CONFIG, CLEAR_BIT(rfReadRegChar(CONFIG), PRIM_RX));
+    _delay_us(130);
+}
+
+// sets the radio to receive mode
+void rfSetToReceiveMode(void)
+{
+    CE_LOW;
+    rfWriteRegChar(CONFIG, SET_BIT(rfReadRegChar(CONFIG), PRIM_RX));
+    _delay_us(130);
+    CE_HIGH;
+}
+
+// sets AutoAck on a pipe
+void rfSetAutoAckForPipe(uint8_t pipeAddress)
+{
+    if (ENAA_P0 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P0));
+    }
+    else if (ENAA_P1 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P1));
+    }
+    else if (ENAA_P2 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P2));
+    }
+    else if (ENAA_P3 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P3));
+    }
+    else if (ENAA_P4 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P4));
+    }
+    else if (ENAA_P5 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, SET_BIT(rfReadRegChar(EN_AA), ENAA_P5));
+    }
+    else
+    {
+        // something wrong
+    }
+}
+
+// Clears autoack on a pipe
+void rfClearAutoAckForPipe(uint8_t pipeAddress)
+{
+    if (ENAA_P0 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P0));
+    }
+    else if (ENAA_P1 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P1));
+    }
+    else if (ENAA_P2 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P2));
+    }
+    else if (ENAA_P3 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P3));
+    }
+    else if (ENAA_P4 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P4));
+    }
+    else if (ENAA_P5 == pipeAddress)
+    {
+        rfWriteRegChar(EN_AA, CLEAR_BIT(rfReadRegChar(EN_AA), ENAA_P5));
+    }
+    else
+    {
+        // something wrong
+    }
+}
+
+// enables an address pipe
+void rfEnablePipeForRx(uint8_t pipeAddress)
+{
+    if (ERX_P0 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P0));
+    }
+    else if (ERX_P1 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P1));
+    }
+    else if (ERX_P2 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P2));
+    }
+    else if (ERX_P3 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P3));
+    }
+    else if (ERX_P4 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P4));
+    }
+    else if (ERX_P5 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, SET_BIT(rfReadRegChar(EN_RXADDR), ERX_P5));
+    }
+    else
+    {
+        // something wrong
+    }
+}
+
+// disable an address pipe
+void rfDisablePipeForRx(uint8_t pipeAddress)
+{
+    if (ERX_P0 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P0));
+    }
+    else if (ERX_P1 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P1));
+    }
+    else if (ERX_P2 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P2));
+    }
+    else if (ERX_P3 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P3));
+    }
+    else if (ERX_P4 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P4));
+    }
+    else if (ERX_P5 == pipeAddress)
+    {
+        rfWriteRegChar(EN_RXADDR, CLEAR_BIT(rfReadRegChar(EN_RXADDR), ERX_P5));
+    }
+    else
+    {
+        // something wrong
+    }
+}
+
+// set number of retries
+void rfSetRetryAttempts(uint8_t attempts)
+{
+    // check for bounds
+    rfWriteRegChar(SETUP_RETR, SET_VALUE(rfReadRegChar(SETUP_RETR), attempts));
+}
+
+// sets the retransmit delays
+void rfSetRetryDelay(uint8_t delay)
+{
+    // check for bounds
+    rfWriteRegChar(SETUP_RETR, SET_VALUE(rfReadRegChar(SETUP_RETR), (delay<<4)));
+}
+
+// make the channel busy by transmitting carrier wave
+void rfTransmitCarrier(void)
+{
+    rfWriteRegChar(RF_SETUP, SET_BIT(rfReadRegChar(RF_SETUP), CONT_WAVE));
+}
+
+// reads the channel power
+uint8_t rfReadChannelPower(void)
+{
+    return 0;
+}
+
+// enables dynamic payload for a pipe
+void rfSetDynamicPayloadForPipe(uint8_t dymamic)
+{
+
+}
+
+// enables dynamic payload for a pipe
+void rfClearDynamicPayloadForPipe(uint8_t dymamic)
+{
+
+}
+
+// enable dynamic payload
+void rfEnableGlobalDynamicPayload(void)
+{
+
+}
+
+// enable Ack payload
+void rfEnableGlobalAckPayload(void)
+{
+
+}
+
+// enables dynamic payload
+void rfEnableGlobalDynamicAckPayload(void)
+{
+
+}
+
+// status about the TX FIFO status
+uint8_t rfTxFifoStatus(void)
+{
+    return 0;
+}
+
+// status about the RX FIFO status
+uint8_t rfRxFifoStatus(void)
+{
+    return 0;
+}
+
+/** \brief
+ *
+ *
+ */
+ISR(EXT_INT)
 {
     uint8_t status;
     status = rfStatus();
--- cRF24.h
+++ cRF24.h
@@ -20,6 +20,11 @@
 
 #include <inttypes.h>
 
+// INT0 external vector
+#define EXT_INT INT0_vect
+// INT1 external vector
+//#define EXT_INT INT1_vect
+
 #define CSN_HIGH CSN_H
 #define CSN_LOW  CSN_L
 #define CE_HIGH  CE_H
@@ -28,6 +33,10 @@
 #define CHECK_BIT(VAL,MASK)    (VAL & (1<<MASK))
 #define SET_BIT(VAL,MASK)      (VAL | (1<<MASK))
 #define CLEAR_BIT(VAL,MASK)    (VAL & ~(1<<MASK))
+
+#define CHECK_VALUE(VAL,MASK)    (VAL & (MASK))
+#define SET_VALUE(VAL,MASK)      (VAL | (MASK))
+#define CLEAR_VALUE(VAL,MASK)    (VAL & ~(MASK))
 
 // handles the transmission of rfPacket
 void rfTransmit(uint8_t* buff, uint8_t len);
@@ -38,13 +47,13 @@ void rfReceived(void);
 // handles the confirmation of transmission
 void rfTransmited(void);
 
-// changes the state of the tranciver to standby mode one
+// changes the state of the transceiver to standby mode one
 void rfToStandbyOne(void);
 
-// changes the state of the tranciver to powerDown mode
+// changes the state of the transceiver to powerDown mode
 void rfToPowerDown(void);
 
-// continously listens
+// continuously listens
 void rfStartListening(void);
 
 // reads the register and writes to the buffer
@@ -76,5 +85,103 @@ uint8_t rfStatus(void);
 
 // stops listening
 void rfStopListening(void);
+
+// flush rx FIFO
+uint8_t rfFlushRx(void);
+
+// flush TX FIFO
+uint8_t rfFlushTx(void);
+
+// rf status
+uint8_t rfStatus(void);
+
+// sets the radio frequency
+void rfSetChannel(uint8_t channel);
+
+// sets the transmit power
+void rfSetTransmitPower(uint8_t power);
+
+// sets the transmit data-rate
+void rfSetDataRate(uint8_t dataRate);
+
+// sets receive address with
+void rfSetAddressWidth(uint8_t addressWidth);
+
+/** \brief Enables CRC
+ *
+ * \return NonReturning
+ *
+ */
+void rfEnableCRC(void);
+
+/** \brief Disables CRC
+ *
+ * \return NonReturning
+ *
+ */
+void rfDisableCRC(void);
+
+// sets the number of crc bytes
+void rfSetCrcScheme(uint8_t scheme);
+
+// sets interrupt mask
+void rfSetInterruptMask(uint8_t mask);
+
+// clears interrupt mask
+void rfClearInterruptMask(uint8_t mask);
+
+// sets the radio to transmit mode
+void rfSetToTransmitMode(void);
+
+// sets the radio to receive mode
+void rfSetToReceiveMode(void);
+
+// sets AutoAck on a pipe
+void rfSetAutoAckForPipe(uint8_t pipeAddress);
+
+// Clears autoack on a pipe
+void rfClearAutoAckForPipe(uint8_t pipeAddress);
+
+// enables an address pipe
+void rfEnablePipeForRx(uint8_t pipeAddress);
+
+// disable an address pipe
+void rfDisablePipeForRx(uint8_t pipeAddress);
+
+// set number of retries
+void rfSetRetryAttempts(uint8_t attempts);
+
+// sets the retransmit delays
+void rfSetRetryDelay(uint8_t delay);
+
+// make the channel busy by transmitting carrier wave
+void rfTransmitCarrier(void);
+
+// reads the channel power
+uint8_t rfReadChannelPower(void);
+
+// enables dynamic payload for a pipe
+void rfSetDynamicPayloadForPipe(uint8_t dymamic);
+
+// enables dynamic payload for a pipe
+void rfClearDynamicPayloadForPipe(uint8_t dymamic);
+
+// enable dynamic payload
+void rfEnableGlobalDynamicPayload(void);
+
+// enable Ack payload
+void rfEnableGlobalAckPayload(void);
+
+// enables dynamic payload
+void rfEnableGlobalDynamicAckPayload(void);
+
+// status about the TX FIFO status
+uint8_t rfTxFifoStatus(void);
+
+// status about the RX FIFO status
+uint8_t rfRxFifoStatus(void);
+
+// writes the rfAckPayload from local buffer
+uint8_t rfWriteAckPayload(const uint8_t* buf, uint8_t len);
 
 #endif // _CRF24_H_
